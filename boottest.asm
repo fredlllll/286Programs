6002706406bf3c5f2d208b418837c287abf380fe
Frederik Gelder <frederik.gelder@freenet.de>
2020-11-15 19:48:40 +0100

    using incs in boottest, fixing 32 bit mul

--- boottest.asm
+++ boottest.asm
@@ -1,9 +1,6 @@
 ; this is a bootloader that only prints a message to the screen.
 ; used to verify that a computer can boot from a floppy
-format binary ; output raw binary
-use16 ; generate 16 bit code
-
-org 7C00h ; we expect our code to be located in this point in memory by default
+include "16bitinit.inc"
 
 start:
 
@@ -27,5 +24,4 @@ write_done:
 szMessage db 'This is a test message, we have boot.'
 lenMessage = $-szMessage
 
-rb 7C00h+512-2-$ ; fill up to the boot record signature, will throw an error on compile if code before it would push out the following signature
-db 055h,0AAh ; signature is expected by some bioses or it will ignore this bootloader
+include "bootloadersig.inc"
--- test.asm
+++ test.asm
@@ -1,32 +1,48 @@
 include "16bitinit.inc"
 start:
-mov ax,0
+
+;30000*30000 = 900000000 = 35A4 E900
+;35A4 = 13732
+;E900 = 59648
+;AX:BX *= CX:DX
+
+;01101101 01011101 01100100 11101001 00000000
+;23908
+
+
+;mov ax,30000
+;mov dx,30000
+;mul dx
+;push dx
+;push ax
+
+;jmp printResults; skip the lib thing
+
+mov ax,10
 mov bx,30000
 mov cx,ax
 mov dx,bx
-;muls AX:BX and CX:DX and stores in AX:BX
 call mul32bit
+push bx
+push ax
+
+printResults:;print stuff on stack
+pop ax
 mov dx,0
 mov si, uitoaOut
 call uitoa16
-call println ; print ax
-mov ax,bx
+call println
+
+pop ax
+mov dx,0
 mov si, uitoaOut
 call uitoa16
-call println ; print bx
-
-mov si,strStartupMessage ; move address of message to si
-mov cx,CONSTlenStartupMessage ; move message length to loop counter
-call println ; print startup message
-
+call println
 
 programEnd:
 hlt
 jmp programEnd
 
-
-strStartupMessage db 'HDD saver 1.0, press Enter to begin'
-CONSTlenStartupMessage = $-strStartupMessage
 uitoaOut db 0,0,0,0,0,0
 
 include "32bitmath.inc"
