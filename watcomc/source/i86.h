79820cc132df3fd54c7582f7bf8b423111281e02
Frederik Gelder <frederik.gelder@freenet.de>
2026-08-25 01:07:53 +0200

    vscode fooling breaks watcom build

--- watcomc/source/i86.h
+++
@@ -1,7 +0,0 @@
-/* Stub for VS Code IntelliSense only.
-   During actual compilation, Watcom's own i86.h is used. */
-#ifndef I86_H_STUB
-#define I86_H_STUB
-#include "intdef.h"
-#define MK_FP(seg, off) ((void __far *)0)
-#endif
